63d5041be9b88695505dcee0d9e79853af51e5ac
fanuel <fanueln@stud.ntnu.no>
2022-04-20 15:43:04 +0200

    Init backend

---
+++ backend/boco/src/main/java/boco/BocoApplication.java
@@ -0,0 +1,13 @@
+package boco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BocoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BocoApplication.class, args);
+    }
+
+}
---
+++ backend/boco/src/test/java/boco/BocoApplicationTests.java
@@ -0,0 +1,13 @@
+package boco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BocoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
