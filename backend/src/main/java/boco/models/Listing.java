3a9ba198125ad14a092c19a43a2e20fa81c3030a
Ask Røsand <askbr@stud.ntnu.no>
2022-04-21 09:24:33 +0200

    Implemented Listing model

--- backend/src/main/java/boco/models/Listing.java
+++ backend/src/main/java/boco/models/Listing.java
@@ -3,11 +3,22 @@ package boco.models;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import java.sql.Timestamp;
 
 @Getter @Setter @NoArgsConstructor
 public class Listing {
-    private int id;
+    private long id; // Primary key
     private String name;
     private String description;
+    private String category;
+    private String address;
+    private boolean isAvailable;
+    private boolean isActive;
+    private double price;
+    private String priceType;
+    private Timestamp lastChanged;
+    private double rating;
+
+
 
 }
