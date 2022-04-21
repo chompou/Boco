ec743d6649958ae84a6c96953b86e2d77f235976
fanuel <fanueln@stud.ntnu.no>
2022-04-21 13:22:09 +0200

    #25 Search field and search by dropdown.

--- frontend/src/views/FrontPageView.vue
+++ frontend/src/views/FrontPageView.vue
@@ -1,18 +1,30 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="frontPageWrap container">
+    <div class="justify-content-center">
+      <label>Search: </label>
+      <input id="searchTxtField" class="mt-3" />
+      <div class="dropdown mt-2">
+        <button
+          class="btn btn-secondary dropdown-toggle"
+          type="button"
+          id="dropdownMenuButton"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+        >
+          Search by:
+        </button>
+        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+          <li><a class="dropdown-item" href="#">Name</a></li>
+          <li><a class="dropdown-item" href="#">Score</a></li>
+          <li><a class="dropdown-item" href="#">Hest</a></li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
-export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
-  },
-};
-</script>
+<style scoped>
+#searchTxtField {
+  width: 300px;
+}
+</style>
