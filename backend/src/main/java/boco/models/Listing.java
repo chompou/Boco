f9c2b563a13fe2599fa183bb2cb8be43d6367dc3
Ask Røsand <askbr@stud.ntnu.no>
2022-04-21 09:08:13 +0200

    Added Listing model

---
+++ backend/src/main/java/boco/models/Listing.java
@@ -0,0 +1,13 @@
+package boco.models;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor
+public class Listing {
+    private int id;
+    private String name;
+    private String description;
+
+}
