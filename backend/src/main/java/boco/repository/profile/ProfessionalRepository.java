4cbe707af31686979003d89bcf2e014ca082da09
Emil Glückstad <emilgl@stud.ntnu.no>
2022-04-21 11:46:18 +0200

    bakend into db plus small fix

--- backend/src/main/java/boco/repository/profile/ProfessionalRepository.java
+++ backend/src/main/java/boco/repository/profile/ProfessionalRepository.java
@@ -1,6 +1,7 @@
 package boco.repository.profile;
 
+import boco.models.profile.Professional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProfessionalRepository extends JpaRepository<ProfessionalRepository, Long> {
+public interface ProfessionalRepository extends JpaRepository<Professional, Long> {
 }
