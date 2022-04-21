697bf8af18f4d8645b775cdd0678924286dc39f0
Oyvibjon <81357022+Oyvibjon@users.noreply.github.com>
2022-04-21 11:15:57 +0200

    RegisterView still needs css

--- frontend/src/views/RegisterView.vue
+++ frontend/src/views/RegisterView.vue
@@ -1,8 +1,9 @@
 <template>
   <h2>Register new user</h2>
-  <div class="mb-3">
+  <div class="registerContainer">
     <label>Username</label>
     <input
+      v-model="username"
       type="text"
       placeholder="Enter username"
       name="username"
@@ -12,6 +13,7 @@
     <br />
     <label>Email</label>
     <input
+      v-model="email"
       type="email"
       placeholder="Enter email"
       name="email"
@@ -21,6 +23,7 @@
     <br />
     <label>Phone number</label>
     <input
+      v-model="phoneNumber"
       type="text"
       placeholder="Enter phone number"
       name="phoneNumber"
@@ -30,6 +33,7 @@
     <br />
     <label>Password</label>
     <input
+      v-model="password"
       type="password"
       placeholder="Enter password"
       name="password"
@@ -39,13 +43,42 @@
     <br />
     <label>Verify Password</label>
     <input
+      v-model="verifyPassword"
       type="password"
-      placeholder="Enter password"
+      placeholder="Verify password"
       name="password"
       id="verifyPassword"
       required
     />
     <br />
-    <button type="submit" class="registrationbtn">Submit</button>
+    <button type="submit" class="registerbutton">Submit</button>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: "",
+      email: "",
+      phoneNumber: "",
+      password: "",
+      verifyPassword: "",
+    };
+  },
+};
+</script>
+
+<style scoped>
+.registerContainer {
+  width: 25%;
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+}
+#registerbutton {
+  display: flex;
+  margin: 0 auto;
+  margin-top: 10px;
+}
+</style>
