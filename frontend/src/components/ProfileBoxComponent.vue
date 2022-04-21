766c6bbb887cc2379a7012147914cecf49447b86
Oyvibjon <81357022+Oyvibjon@users.noreply.github.com>
2022-04-21 15:11:52 +0200

    Merge branch 'frontend_ø' into frontend

---
+++ frontend/src/components/ProfileBoxComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="profileBox">
+    <h2>{{ username }}</h2>
+    <p>{{ phoneNumber }} || {{ email }}</p>
+  </div>
+  <div>
+    <p>Map</p>
+    <!--MAP-->
+    <p>Rating</p>
+    <!--RATING-->
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: "test",
+      phoneNumber: "66666666",
+      email: "test@test.com",
+    };
+  },
+};
+</script>
