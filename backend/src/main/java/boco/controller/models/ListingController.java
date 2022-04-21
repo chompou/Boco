2cbaf944d29f64cf6b61e429291377231a756872
BiasedLiar <Bretagard12345>
2022-04-21 13:17:47 +0200

    started controllers

--- backend/src/main/java/boco/controller/models/ListingController.java
+++ backend/src/main/java/boco/controller/models/ListingController.java
@@ -1,9 +1,27 @@
 package boco.controller.models;
 
+import boco.models.rental.Listing;
+import boco.service.rental.ListingService;
+import org.hibernate.annotations.Fetch;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/listing")
 public class ListingController {
+    private final ListingService listingService;
+
+    @Autowired
+    public ListingController(ListingService listingService) {
+        this.listingService = listingService;
+    }
+
+    @GetMapping
+    public List<Listing> getLists(){
+        return listingService
+    }
 }
