59af9effe433d962d0522d4ba20c161e6f0c3b39
Ask Røsand <askbr@stud.ntnu.no>
2022-04-21 09:24:41 +0200

    Implemented CategoryTypes model

---
+++ backend/src/main/java/boco/models/CategoryTypes.java
@@ -0,0 +1,11 @@
+package boco.models;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor
+public class CategoryTypes {
+    private long id; // Primary key
+    private String name;
+}
