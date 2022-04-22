70821c56aa5d0e5f5cfbecd692f9b2550d5241c1
Jon Martin Kristiansen <jonmkr@stud.ntnu.no>
2022-04-22 14:17:34 +0200

    Updated api service with more endpoints

--- frontend/src/services/apiService.js
+++ frontend/src/services/apiService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import storageService from "./storageService";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:8888",
+  baseURL: "http://localhost:8080/api",
   timeout: 1000,
   headers: {
     Accept: "application/json",
@@ -15,11 +15,35 @@ axios.interceptors.request.use((config) => {
 });
 
 export default {
-  getProfile(profileID) {
-    return apiClient.get("/profile/" + profileID);
+  getItem(itemId) {
+    return apiClient.get("/listing/" + itemId);
+  },
+
+  getItems(filters, page, perPage) {
+    return apiClient.get("/listing", {
+      params: { ...filters, page: page, perPage: perPage },
+    });
+  },
+
+  getReviews(filters, page, perPage) {
+    return apiClient.get("/reviews", {
+      params: { ...filters, page: page, perPage: perPage },
+    });
+  },
+
+  getProfile(profileId) {
+    return apiClient.get("/profile/" + profileId);
+  },
+
+  createProfile(profile) {
+    return apiClient.post("/profile", profile);
   },
 
   login(username, password) {
     return apiClient.post("/login", { username: username, password: password });
+  },
+
+  getMyProfile() {
+    return apiClient.get("/my/profile");
   },
 };
