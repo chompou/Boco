fadc104e86d8806c7c9558fa29cdef2013d7d24e
Jon Martin Kristiansen <jonmkr@stud.ntnu.no>
2022-04-27 10:25:21 +0200

    fixed login state persistence, increased to 1h

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -21,7 +21,11 @@
               <router-link to="login" v-if="!$store.state.loggedIn"
                 ><font-awesome-icon icon="right-to-bracket"
               /></router-link>
-              <button id="log-out-button" v-if="$store.state.loggedIn">
+              <button
+                id="log-out-button"
+                v-if="$store.state.loggedIn"
+                @click="logout"
+              >
                 <font-awesome-icon icon="sign-out-alt" />
               </button>
             </a>
@@ -72,6 +76,20 @@
     <div style="height: 20px" />
   </div>
 </template>
+
+<script>
+import storageService from "./services/storageService";
+export default {
+  methods: {
+    logout() {
+      storageService.clearToken();
+      this.$store.state.loggedIn = false;
+      this.$store.state.loggedInUser = null;
+      this.$router.push("/");
+    },
+  },
+};
+</script>
 
 <style>
 :root {
