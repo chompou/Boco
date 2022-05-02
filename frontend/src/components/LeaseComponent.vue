b5f82c0f3602614c05e94d54804fd7f731d07bf0
Jon Martin Kristiansen <jonmkr@stud.ntnu.no>
2022-05-02 13:07:30 +0200

    components for lease view

---
+++ frontend/src/components/LeaseComponent.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="lease-container">
+    <div style="flex-direction: row">
+      <h4>{{ lease.title }}</h4>
+      <h6>Leased by {{ lease.owner }}</h6>
+      <h6>{{ lease.from }} - {{ lease.to }}</h6>
+    </div>
+    <div>
+      <h5>Lease Ended</h5>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["lease"],
+};
+</script>
+
+<style scoped>
+.lease-container {
+  display: flex;
+  border: 3px solid;
+  border-radius: 10px;
+  padding: 15px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  text-align: left;
+  gap: 50px;
+  cursor: pointer;
+}
+
+.lease-container:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+</style>
---
+++ frontend/src/components/LeaseDetailComponent.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="lease-detail-modal">
+    <div class="lease-detail-container">
+      <h2>Item: {{ lease.title }}</h2>
+      <h3>Owner: {{ lease.owner }}</h3>
+      <h3>Leaser: {{ lease.leaser }}</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["lease"],
+};
+</script>
+
+<style scoped>
+.lease-detail-modal {
+  position: fixed;
+  z-index: 9999;
+  top: 20%;
+  left: 50%;
+  transform: translateX(-50%);
+
+  height: 60%;
+  width: 50%;
+
+  border: solid 1px;
+  border-radius: 5px;
+  background-color: white;
+}
+
+.lease-detail-container {
+  display: flex;
+  justify-content: center;
+}
+</style>
