627fe10a1cb79882cc59e0780be8ef091ffb3b87
Oyvibjon <81357022+Oyvibjon@users.noreply.github.com>
2022-05-05 12:29:32 +0200

    Update loginForm.spec.js

--- frontend/tests/unit/loginForm.spec.js
+++ frontend/tests/unit/loginForm.spec.js
@@ -1,9 +1,9 @@
-import { mount, shallowMount } from "@vue/test-utils";
+import { shallowMount } from "@vue/test-utils";
 import LoginView from "@/views/LoginView";
 import store from "@/store";
 import router from "@/router";
 
-describe("Loginform", () => {
+describe("Login form", () => {
   it("Wrong credentials message is visible if login fail", async () => {
     const wrapper = shallowMount(LoginView);
     await wrapper.setData({
@@ -15,12 +15,30 @@ describe("Loginform", () => {
 
   it("Check that Login message renders", () => {
     const loginTitle = "Please login!";
-    const wrapper = mount(LoginView, {
+    const wrapper = shallowMount(LoginView, {
       global: {
         plugins: [store, router],
       },
     });
     const loginMessage = wrapper.find("#login-label");
     expect(loginMessage.text()).toMatch(loginTitle);
+  });
+
+  test("Check that input is working", async () => {
+    const wrapper = shallowMount(LoginView, {
+      global: {
+        plugins: [store, router],
+      },
+    });
+    const username = "user";
+    const password = "pass";
+    const usernameInput = wrapper.find("#username");
+    const passwordInput = wrapper.find("#password");
+
+    await usernameInput.setValue("user");
+    await passwordInput.setValue("pass");
+
+    expect(usernameInput.element.value).toEqual(username);
+    expect(passwordInput.element.value).toEqual(password);
   });
 });
