b167271f0e00d13611b098daac0f3adff58d9d76
Ask Røsand <askbr@stud.ntnu.no>
2022-05-06 18:38:37 +0200

    Javadoc

--- backend/src/main/java/boco/repository/profile/ProfessionalRepository.java
+++ backend/src/main/java/boco/repository/profile/ProfessionalRepository.java
@@ -4,6 +4,9 @@ import boco.model.profile.Professional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Repository for Professional
+ */
 @Repository
 public interface ProfessionalRepository extends JpaRepository<Professional, Long> {
 }
