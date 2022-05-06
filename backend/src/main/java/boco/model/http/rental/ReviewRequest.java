12b4bd69f6572dc49dc60fc0b13fa836de66443a
BiasedLiar <Bretagard12345>
2022-05-06 15:36:57 +0200

    Added javadoc to http models

--- backend/src/main/java/boco/model/http/rental/ReviewRequest.java
+++ backend/src/main/java/boco/model/http/rental/ReviewRequest.java
@@ -2,6 +2,11 @@ package boco.model.http.rental;
 
 import lombok.*;
 
+/**
+ * This class represents a review when sending an HTTP POST request to a REST controller.
+ *
+ * See boco.model.rental.Review class for documentation of the fields of this class.
+ */
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
 public class ReviewRequest {
     private Double rating; // Rating 1-5
