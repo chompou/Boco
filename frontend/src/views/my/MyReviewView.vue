9b8bfa437b90bfb827e0f545b1301e9e5e931781
Sindre <sindre.glomnes@gmail.com>
2022-05-06 18:23:55 +0200

    profilerating.

--- frontend/src/views/my/MyReviewView.vue
+++ frontend/src/views/my/MyReviewView.vue
@@ -1,23 +1,23 @@
 <template>
   <div class="container">
     <div class="rating-container">
-      <h2>Rating</h2>
+      <h2 class="headerr">Rating</h2>
       <div class="rating-box-container">
         <div class="items">
           <p class="ratingText">Lease out</p>
-          <RatingComponent :rating="5" />
+          <RatingComponent :rating="this.ratingAsLease" />
         </div>
         <div class="items">
           <p class="ratingText">Overall</p>
-          <RatingComponent :rating="3" />
+          <RatingComponent :rating="this.ratingAsOwner" />
         </div>
         <div class="items">
           <p class="ratingText">Lease in</p>
-          <RatingComponent :rating="0" />
+          <RatingComponent :rating="this.ratingListing" />
         </div>
       </div>
     </div>
-    <h2>Reviews</h2>
+    <h2 class="headerr">Reviews</h2>
     <div class="review-list-container">
       <div class="review-container" v-for="review in reviews" :key="review">
         <b>{{ review.name }}</b>
@@ -30,16 +30,28 @@
 
 <script>
 import RatingComponent from "@/components/RateReview/RatingComponent.vue";
+import apiService from "@/services/apiService";
 export default {
   components: { RatingComponent },
 
   data() {
     return {
       reviews: [],
+      myProfile: null,
+      ratingAsOwner: null,
+      ratingAsLease: null,
+      ratingListing: null,
     };
   },
 
-  created() {},
+  created() {
+    apiService.getMyProfile().then((response) => {
+      this.myProfile = response.data;
+      this.ratingAsOwner = this.myProfile.ratingAsOwner;
+      this.ratingAsLease = this.myProfile.ratingAsLease;
+      this.ratingListing = this.myProfile.ratingListing;
+    });
+  },
 };
 </script>
 
@@ -48,6 +60,10 @@ export default {
   display: flex;
   flex-direction: column;
   gap: 50px;
+}
+
+.headerr {
+  text-align: center;
 }
 
 .rating-box-container {
