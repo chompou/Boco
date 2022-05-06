b21e046572b1c594764c53ba01c9aaa865ecdb0c
Ask Røsand <askbr@stud.ntnu.no>
2022-05-06 10:39:37 +0200

    Javadoc for repositories

--- backend/src/main/java/boco/repository/profile/NotificationRepository.java
+++ backend/src/main/java/boco/repository/profile/NotificationRepository.java
@@ -6,12 +6,26 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+/**
+ * Repository for notification entity
+ */
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    /**
+     * @param profileId ID of the profile
+     * @return Number of unread notifications for a profile with profileId
+     */
     int countByProfileIdAndIsReadFalse(Long profileId);
-    int countByProfileId(Long profileId);
-    List<Notification> findByProfileIdAndIsReadTrue(Long profileId);
-    List<Notification> findByProfileIdAndIsReadFalse(Long profileId);
+
+    /**
+     * @param username unique username of the profile
+     * @return List of read notifications of a profile defined by username
+     */
     List<Notification> findByProfileUsernameAndIsReadTrue(String username);
+
+    /**
+     * @param username unique username of the profile
+     * @return List of unread notifications of a profile defined by username
+     */
     List<Notification> findByProfileUsernameAndIsReadFalse(String username);
 }
