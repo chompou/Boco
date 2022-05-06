f3729ad37b950784231ef88b6bfeb789d84e67b8
Ask Røsand <askbr@stud.ntnu.no>
2022-05-06 09:15:44 +0200

    Javadoc to profile related models

--- backend/src/main/java/boco/model/profile/Profile.java
+++ backend/src/main/java/boco/model/profile/Profile.java
@@ -10,46 +10,52 @@ import javax.persistence.*;
 import java.sql.Timestamp;
 import java.util.List;
 
+/**
+ * JPA entity representing a profile. A profile is either personal or professional.
+ * Personal and professional inherits this class.
+ */
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
 public class Profile {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long id; // Unique ID of the profile
     @Column(unique = true)
-    private String username;
+    private String username; // Unique username
     @Column(unique = true)
-    private String email;
-    private String description;
-    private String displayName;
-    private String passwordHash;
-    private String location;
-    private String address;
-    private Boolean isVerified;
-    private String tlf;
-    private Double ratingListing;
-    private Double ratingAsOwner;
-    private Double ratingAsLeasee;
-    //Time profile was deactivated by user
-    private Timestamp deactivated;
+    private String email; // Unique email
+    private String description; // Description of profile, written by the profile
+    private String displayName; // Name to display
+    private String passwordHash; // Hashed password
+    private String location; // Coordinates of profile on format latitude:longitude
+    private String address; // Address of profile
+    private Boolean isVerified; // Boolean indicating if the profile is verified
+    private String tlf; // Phone number of profile
+    private Double ratingListing; // Average rating of the items (leases) posted by the profile
+    private Double ratingAsOwner; // Average rating given to the profile as owner role in a lease
+    private Double ratingAsLeasee; // Average rating given to the profile as a leasee role in a lease
+    private Timestamp deactivated; // Time profile was deactivated by user. Null: not deactivated
 
     @OneToMany(mappedBy = "profile")
     @JsonBackReference
-    private List<Lease> rentals;
+    private List<Lease> rentals; // List of leases where the profile is a leasee
 
     @OneToMany(mappedBy = "profile")
     @JsonBackReference
-    private List<Lease> owned;
+    private List<Lease> owned; // List of leases where the profile is an owner
 
     @OneToMany(mappedBy = "profile")
     @JsonBackReference
-    private List<Listing> listings;
+    private List<Listing> listings; // List of listings posted by profile
 
     @OneToMany(mappedBy = "profile")
     @JsonBackReference
-    private List<Notification> notifications;
+    private List<Notification> notifications; // Notifications for profile
 
+    /**
+     * Constructor of this class. isVerified is set to false and deactivated is set to null by default.
+     */
     public Profile(String username, String email, String description, String displayName,
                    String passwordHash, String address, String location, String tlf){
         this.username = username;
